0fcbfb98543aa1e0398f8ab3fecef10b9beb5f53
taZa0912 <dlaydwls0912@gmail.com>
2022-03-13 02:06:57 +0900

    파일 삭제

--- BOJ(Baekjoon_Online_Judge)/baekjoon_16431.py
+++
@@ -1,7 +0,0 @@
-from sys import stdin
-
-B = list(map(int, stdin.readline().split()))
-D = list(map(int, stdin.readline().split()))
-J = list(map(int, stdin.readline().split()))
-
-if (abs(B[0]-J[0])+abs)
